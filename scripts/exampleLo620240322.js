41ee971e22387cdfc2efe5d5d257b482b4492313
Azmin-Tahmina <123420809+Azmin-Tahmina@users.noreply.github.com>
2024-03-22 15:33:05 -0600

    Example Created and Updated 2024/03/22

---
+++ scripts/exampleLo620240322.js
@@ -0,0 +1,22 @@
+"use strict"
+
+// $("#italic").click(function (){
+//     let text = "Hello "+ $(this).text() +" !";
+//     //$(this).text() +" !";
+//     alert(text);
+// });
+
+// let testVarible = document.getElementById("italic");
+// alert(testVarible.innerText)
+
+$(document).ready(function(){
+    $("#italic").click(function (){
+        let text = "Hello "+ $(this).text() +" !";
+        //$(this).text() +" !";
+        alert(text);
+    });
+});
+
+// $(function (){
+//
+// });
